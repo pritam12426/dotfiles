a1234853328cc904d5f486e726334cb7f258ce2c
Pritam <84720825+pritam12426@users.noreply.github.com>
2026-01-22 10:41:44 +0530

    I hvae added a new file

--- darwin/config/zsh/alias.zsh
+++ darwin/config/zsh/alias.zsh
@@ -26,12 +26,13 @@
 
 
 # --- HOMEBREW ---------------------
-alias bup='brew update && brew upgrade && brew cleanup --prune=all -s'  # Update installed Homebrew formulae
-alias blc='brew livecheck -q --newer-only --tap alhadis/troff'          # Run livecheck for `alhadis/troff` tap
+alias bup="brew update && brew upgrade && brew cleanup --prune=all -s"  # Update installed Homebrew formulae
+alias blc="brew livecheck -q --newer-only --tap alhadis/troff"          # Run livecheck for `alhadis/troff` tap
 # Display information about Homebrew formulae
-alias bino='brew info'
-alias bi='brew install'
-alias bd='brew desc'
+alias b="brew"
+alias bi="b install"
+alias bd="b desc"
+alias bino="b info"
 
 # --- Directory navigation aliases ---
 alias lldir="cd \` sk < $CPP_LIB_DIR/index.txt \`"     # Navigate To Directory From Index File
@@ -49,31 +50,28 @@ alias .con="cd ~/.config"                              # Navigate To your dotfil
 # alias firefox="open -a /Applications/Firefox.app"   # Launch Firefox
 # alias firefox="/Applications/Firefox.app/Contents/MacOS/firefox"
 alias firefox-p="firefox --private-window"          # Launch Firefox in private mode
-
-alias firefox-clean="rm -fv ~/Library/Application\ Support/Firefox/Profiles/*/formhistory.sqlite ; \
-					 rm -fv ~/Library/Application\ Support/Firefox/Profiles/*/formhistory.dat"
 # ----------------------------------------------------------
 
 
 # ------------ LS and File Management Aliases ------------
 # Enable colorized output for common commands
-alias ls="ls --color=auto -GFh"
-alias grep="grep -i --color=auto"
-alias fgrep="fgrep -i --color=auto"
-alias egrep="egrep -i --color=auto"
-alias rsync="rsync -vrPlu"
+alias ls='ls --color=auto -GFh'
+alias grep='grep -i --color=auto'
+alias fgrep='fgrep -i --color=auto'
+alias egrep='egrep -i --color=auto'
+alias rsync='rsync -vrPlu'
 
 # Common ls shortcuts
-alias ll="ls -l"                       # List with human-readable sizes
-alias la="ls -A"                       # List all files, excluding . and ..
-alias l="ls -lA"                       # Detailed list including hidden files
-alias lh="ls -ld --color=auto .[^.]*"  # List hidden directories
+alias ll='ls -l'                       # List with human-readable sizes
+alias la='ls -A'                       # List all files, excluding . and ..
+alias l='ls -lA'                       # Detailed list including hidden files
+alias lh='ls -ld --color=auto .[^.]*'  # List hidden directories
 
 # File operation aliases with safety prompts
-alias cp="cp -ip"        # Copy with interactive prompt
-alias mv="mv -vi"        # Move with interactive prompt
-alias du="du -hs"        # Display disk usage in human-readable format
-alias bc="bc --quiet -l" # The command like cal Calculator
+alias cp='cp -ip'        # Copy with interactive prompt
+alias mv='mv -vi'        # Move with interactive prompt
+alias du='du -hs'        # Display disk usage in human-readable format
+alias bc='bc --quiet -l' # The command like cal Calculator
 alias df='df -h'
 alias mime='file --mime --mime-type'
 alias scp='scp -pr'
@@ -88,7 +86,7 @@ alias enrc="$EDITOR ~/.config/nvim/init.lua"          # Edit NeoVim config
 alias enhc="$EDITOR ~/.config/helix/config.toml"      # Edit helix config
 alias eza="$EDITOR  ~/.config/zsh/alias.zsh"          # Edit alias file
 alias efz="$EDITOR  ~/.config/zsh/functions.sh"       # Edit functon file
-alias .e="edot"                                       # Edit functon file
+alias .e='edot'                                       # Edit functon file
 # ---------------------------------------------------------
 
 # Generate nicer-looking hexadecimal dumps
@@ -99,15 +97,15 @@ alias hexdump='hexdump -v \
 	-e '\''16/1 "%_p" "'\''"[2m│[22m"'\''\n"'\'
 
 
-alias umount="diskutil unmount"                     # Apple recommend diskutil(1) be used instead of umount(1)
-alias unquarantine="xattr -rd com.apple.quarantine" # Remove annoying extended attributes added to downloads
+alias umount='diskutil unmount'                     # Apple recommend diskutil(1) be used instead of umount(1)
+alias unquarantine='xattr -rd com.apple.quarantine' # Remove annoying extended attributes added to downloads
 
 alias typecode='GetFileInfo -t'    # Print 4-character creator/type codes
 alias creatorcode='GetFileInfo -c' # Print 4-character creator/type codes
 
 # Resize Terminal.app to fill the screen
-alias fit='printf '\''\e[3;0;0t\e[4;0;9999t'\'
-alias FIT='for i in /dev/ttys???; do fit > "$i"; done'
+alias fit="printf "\""\e[3;0;0t\e[4;0;9999t"\"
+alias FIT="for i in /dev/ttys???; do fit > "$i"; done"
 
 # System utility aliases ------------------------------
 alias o="open ."                                                                           # Open current directory in Finder
@@ -117,23 +115,23 @@ alias sqldump='sqlite3 /dev/stdin .dump <'                                      
 alias prune='find -L . -name . -o -type d -prune -o -type l -exec rm -v {} +'              # Delete broken symlinks in the current directory
 alias hardlinks='find . \! -type d \! -links 1'                                            # List files with at least one hard-link
 alias xxd='NO_COLOR=1 xxd -u -g1'                                                          # Generate nicer-looking hexadecimal dumps
-alias peek="qlmanage -p >/dev/null 2>&1  -- "                                              # Preview a file using Quick Look
-alias pow="pmset -g batt" 	                                                               # Print power diagnostics (battery-level and charge status)
-alias tree="tree -a --noreport"                                                            # Hide summary lines in tree(1) output
+alias peek='qlmanage -p >/dev/null 2>&1  -- '                                              # Preview a file using Quick Look
+alias pow='pmset -g batt' 	                                                               # Print power diagnostics (battery-level and charge status)
+alias tree='tree -a --noreport'                                                            # Hide summary lines in tree(1) output
 alias bush='tree -spugDF --metafirst --timefmt="%Y-%m-%d %T"'
-# alias live-server="open 'http://localhost:8085/' && python3 -m http.server 8085"           # Start live server with python
-alias live-server="live-server -H localhost -p 8085 -o"                                    # Start live server with rust binary
+# alias live-server='open 'http://localhost:8085/' && python3 -m http.server 8085'           # Start live server with python
+alias live-server='live-server -H localhost -p 8085 -o'                                    # Start live server with rust binary
 # alias tre="tre -a -E '.git'"                                                               # Display directory tree
-# alias python="python3 -u"                                                                  # Use Python 3 as default
-alias envpath="envpath | less"                                                             # Print the environment variable in prettiest form
-alias nq="networkquality -s"                                                               # Check network quality
-alias search="command ls -AF | grep -i"                                                    # Search files in current directory
-alias sayy="pbpaste | command say -i"                                                      # Convert clipboard text to speech
-alias per="find . -type f -exec chmod 644 {} \; ; find . -type d -exec chmod 755 {} \;"    # Fix permissions
-alias seelog="tail -n 1 -f --"                                                             # Tail logs
+# alias python='python3 -u'                                                                  # Use Python 3 as default
+alias envpath='envpath | less'                                                             # Print the environment variable in prettiest form
+alias nq='networkquality -s'                                                               # Check network quality
+alias search='command ls -AF | grep -i'                                                    # Search files in current directory
+alias sayy='pbpaste | command say -i'                                                      # Convert clipboard text to speech
+alias per='find . -type f -exec chmod 644 {} \; ; find . -type d -exec chmod 755 {} \;'    # Fix permissions
+alias seelog='tail -n 1 -f --'                                                             # Tail logs
 alias exportlib="source $LIBS_DIR/env"                                                     # Load library environment
-alias bsdtar="bsdtar --acls --fflags --xattrs --mac-metadata"                              # Archive macOS-specific filesystem attributes
-alias zzz="bsdtar -vxf"                                                                    # Extract archives
+alias bsdtar='bsdtar --acls --fflags --xattrs --mac-metadata'                              # Archive macOS-specific filesystem attributes
+alias zzz='bsdtar -vxf'                                                                    # Extract archives
 alias exportembdlib="source $DOT_FILE/../global/embedded/embedded-ENV.sh"                  # Load embedded environment
 alias ninja-tree="ninja -t targets"                                                        # Display Ninja build targets
 alias chownroot="sudo chown -R root:wheel"                                                 # Change ownership to root
@@ -155,8 +153,8 @@ alias reload-aria2c="launchctl unload ~/Library/LaunchAgents/com.user.aria2.plis
 # --------------------------------------------------
 
 # ------------ EXIFTool Aliases ------------
-alias exf="exiftool -sort -P -overwrite_original_in_place" # Modify metadata
-alias exfcpy="exf -TagsFromFile"                           # Copy metadata from another file
+alias exf='exiftool -sort -P -overwrite_original_in_place' # Modify metadata
+alias exfcpy='exf -TagsFromFile'                           # Copy metadata from another file
 alias stripmeta='exiftool -All= -overwrite_original'
 # alias x='exiftool -a -U'
 # alias X='x -b -X'
@@ -164,11 +162,11 @@ alias stripmeta='exiftool -All= -overwrite_original'
 
 # IP and MAC Address Aliases =================================================================================
 # Fetch public IP information
-alias ipinfo="curl https://raw.githubusercontent.com/jarun/nnn/master/plugins/ipinfo 2> /dev/null | sh | less"
+alias ipinfo='curl https://raw.githubusercontent.com/jarun/nnn/master/plugins/ipinfo 2> /dev/null | sh | less'
 
 # Fetch WAN (public) IP address
 alias myip="curl -s http://checkip.dyndns.org/ | sed 's/[a-zA-Z<>/ :]//g'"
 
 # Fetch LAN (local) IP address
-alias lanip="ipconfig getifaddr en0"
+alias lanip='ipconfig getifaddr en0'
 # ============================================================================================================
