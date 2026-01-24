d6bbe3d32b8d0455b721f3b05624aec6da2ba7c7
Pritam <84720825+pritam12426@users.noreply.github.com>
2026-01-24 16:52:05 +0530

    I have commed some files

--- darwin/config/newsboat/scripts/bookmark.sh
+++ darwin/config/newsboat/scripts/bookmark.sh
@@ -5,8 +5,10 @@
 url=$(curl -sIL -o /dev/null -w '%{url_effective}' "$1")
 
 url=$(echo "${url}" | perl -p -e 's/(\?|\&)?utm_[a-z]+=[^\&]+//g;' -e 's/(#|\&)?utm_[a-z]+=[^\&]+//g;')
-
 title="$2"
 description="$3"
 
-grep -q "${url}\t${title}\t${description}" ~/.config/newsboat/bookmarks_temp.txt || printf  "%s\t%s\t%s" "${url}" "${title}" "${description}" >> ~/.config/newsboat/bookmarks_temp.txt
+LINE=$(printf "%s\t%s\t%s" "${url}" "${title}" "${description}")
+bookMark_file="$HOME/.local/share/newsboat/bookmarks.txt"
+
+grep -q "$LINE" "$bookMark_file" || echo "$LINE" >> "$bookMark_file"
